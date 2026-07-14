baa35a6d9dff53e8565696941fa191026e870402
syifaamdh <syifaamdh@gmail.com>
2026-07-14 16:53:43 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import { FaNodeJs } from "react-icons/fa"
-import HomePage from "./HomePage"
-// import { ref, get } from "firebase/database"
-// import { database } from "@/firebase"
+import HomePage from "./HomePage";
+import data from "@/data.json";
 
-async function getData() {
-
-  // return await (await get(ref(database))).val()
-
-  const DB_URL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL + '/.json'
-  const res = await fetch(DB_URL, { cache: 'no-store' })
-  const data = res.json()
-  return data
+export default function Page() {
+  return <HomePage data={data} />;
 }
 
 export default async function page() {
